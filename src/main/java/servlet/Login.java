1581f928bf8f6eea344c3915d0f96b89e46f6c64
r-kcp <r6170.s@gmail.com>
2025-10-29 16:24:38 +0900

    ファイル追加

---
+++ src/main/java/servlet/Login.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.LoginLogic;
+import model.User;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		
+		//Userインスタンス（ユーザー情報）の生成
+		User user = new User(pass,name);
+		
+		//ログイン処理
+		LoginLogic loginlogic = new LoginLogic();
+		boolean isLogin = loginlogic.execute(user);
+		
+		//ログイン成功時
+		if(isLogin) {
+			//ユーザー情報をセッションに保存
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginResult.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+//
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+//
+
+
+}
